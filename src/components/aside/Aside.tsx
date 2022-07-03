276321223f1fc0e1963ddd0017132adc5adf3641
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-07-03 18:00:18 -0400

    Finishing aside

--- src/components/aside/Aside.tsx
+++ src/components/aside/Aside.tsx
@@ -20,13 +20,40 @@ const Aside: React.FC = () => {
           <div className="topic">Tecnologia - Assuntos do momento</div>
           <div className="title">#ELONSTWITTER</div>
           <div className="count">13.9 mil Tweets</div>
-
         </div>
         <div className="trading-topic">
           <div className="topic">Tecnologia - Assuntos do momento</div>
           <div className="title">#FREECOFFEE</div>
           <div className="count">23.5 mil Tweets</div>
-
+        </div>
+        <div className="show-more">
+          <a href="">Mostrar mais</a>
+        </div>
+      </div>
+      <div className="new-connections">
+        <div className="connection">
+          <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" />
+          <div className="flex flex-col text-slate-200">
+            <span className="font-bold">Username</span>
+            <span className="brightness-75 font-thin">@usertag</span>
+          </div>
+          <button className="follow">Seguir</button>
+        </div>
+        <div className="connection">
+          <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" />
+          <div className="flex flex-col text-slate-200">
+            <span className="font-bold">Username</span>
+            <span className="brightness-75 font-thin">@usertag</span>
+          </div>
+          <button className="follow">Seguir</button>
+        </div>
+        <div className="connection">
+          <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" />
+          <div className="flex flex-col text-slate-200">
+            <span className="font-bold">Username</span>
+            <span className="brightness-75 font-thin">@usertag</span>
+          </div>
+          <button className="follow">Seguir</button>
         </div>
         <div className="show-more">
           <a href="">Mostrar mais</a>
