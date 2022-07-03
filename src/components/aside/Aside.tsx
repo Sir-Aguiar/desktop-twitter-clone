a1cfa793dbf9ced03446c8e1036aac4faba500a8
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-07-03 16:35:07 -0400

    Search-bar finishing

--- src/components/aside/Aside.tsx
+++ src/components/aside/Aside.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
-
+import React from "react";
+import "../../styles/aside.css";
+import { Search } from "../icons/Icons";
 
 const Aside: React.FC = () => {
-  return <aside></aside>
-}
+  return (
+    <aside className="main-aside">
+      <div className="search-bar">
+        <Search />
+        <input type="text" placeholder="Buscar no Twitter" />
+      </div>
+      <div className="about-now">
+        
+      </div>
+    </aside>
+  );
+};
 
 export default Aside;
--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-
+import "../../styles/main.css"
 const Main: React.FC = () => {
-  return <main></main>;
+  return <main className="twitter-main"></main>;
 };
 
 export default Main;
