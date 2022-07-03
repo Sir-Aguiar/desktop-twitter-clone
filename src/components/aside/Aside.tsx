b09841691e773b98c74825017b162f5801f34309
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-07-03 17:35:15 -0400

    Finishing trading

--- src/components/aside/Aside.tsx
+++ src/components/aside/Aside.tsx
@@ -10,7 +10,27 @@ const Aside: React.FC = () => {
         <input type="text" placeholder="Buscar no Twitter" />
       </div>
       <div className="about-now">
-        
+        <h1 className="py-2 px-5 font-bold text-xl">O que está acontecendo</h1>
+        <div className="trading-topic">
+          <div className="topic ">Tecnologia - Assuntos do momento</div>
+          <div className="title">#VITEISAMAZING</div>
+          <div className="count">33.3 mil Tweets</div>
+        </div>
+        <div className="trading-topic">
+          <div className="topic">Tecnologia - Assuntos do momento</div>
+          <div className="title">#ELONSTWITTER</div>
+          <div className="count">13.9 mil Tweets</div>
+
+        </div>
+        <div className="trading-topic">
+          <div className="topic">Tecnologia - Assuntos do momento</div>
+          <div className="title">#FREECOFFEE</div>
+          <div className="count">23.5 mil Tweets</div>
+
+        </div>
+        <div className="show-more">
+          <a href="">Mostrar mais</a>
+        </div>
       </div>
     </aside>
   );
