2b0bcf670a5d808d55c45900bcd400bed0dbcc21
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-07-05 16:00:40 -0400

    Finishing twitter page

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../../styles/main.css";
 import Aside from "../aside/Aside";
-import { Calendar, Emoji, Enquete, GIF, Location, Media } from "../icons/Icons";
+import { Calendar, Coment, Emoji, Enquete, GIF, Like, Location, Media, Retweet, Upload } from "../icons/Icons";
 const Main: React.FC = () => {
   return (
     <main className="twitter-main">
@@ -15,13 +15,126 @@ const Main: React.FC = () => {
             <div className="icons">
               <Media /> <GIF /> <Enquete /> <Emoji /> <Calendar /> <Location />
             </div>
-            <button disabled className="sm-tweet-button">Tweetar</button>
+            <button disabled className="sm-tweet-button">
+              Tweetar
+            </button>
           </footer>
         </div>
         <div className="user-tweet">
-          <div className="tweet-header"></div>
-          <div className="tweet-body"></div>
-          <div className="tweet-footer"></div>
+          <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" className="tweet-author" />
+          <div className="tweet-body">
+            <div className="tweet-header">
+              <span className="text-slate-200 font-bold">Username</span>
+              <span>@usertag</span>
+              <span>1 h</span>
+            </div>
+            <div className="tweet-content">
+              <p className="tweet">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum numquam voluptatem nostrum atque vel!
+                Quasi ad optio velit obcaecati veniam odio aliquid sunt blanditiis, minus facilis dignissimos
+                repudiandae saepe sed!
+              </p>
+              <div className="tweet-icons">
+                <span className="coment">
+                  <Coment />0
+                </span>{" "}
+                <span className="retweet">
+                  <Retweet />0
+                </span >{" "}
+                <span className="like">
+                  <Like />0
+                </span>{" "}
+                <Upload />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="user-tweet">
+          <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" className="tweet-author" />
+          <div className="tweet-body">
+            <div className="tweet-header">
+              <span className="text-slate-200 font-bold">Username</span>
+              <span>@usertag</span>
+              <span>1 h</span>
+            </div>
+            <div className="tweet-content">
+              <p className="tweet">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum numquam voluptatem nostrum atque vel!
+                Quasi ad optio velit obcaecati veniam odio aliquid sunt blanditiis, minus facilis dignissimos
+                repudiandae saepe sed!
+              </p>
+              <div className="tweet-icons">
+                <span className="coment">
+                  <Coment />0
+                </span>{" "}
+                <span className="retweet">
+                  <Retweet />0
+                </span >{" "}
+                <span className="like">
+                  <Like />0
+                </span>{" "}
+                <Upload />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="user-tweet">
+          <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" className="tweet-author" />
+          <div className="tweet-body">
+            <div className="tweet-header">
+              <span className="text-slate-200 font-bold">Username</span>
+              <span>@usertag</span>
+              <span>1 h</span>
+            </div>
+            <div className="tweet-content">
+              <p className="tweet">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum numquam voluptatem nostrum atque vel!
+                Quasi ad optio velit obcaecati veniam odio aliquid sunt blanditiis, minus facilis dignissimos
+                repudiandae saepe sed!
+              </p>
+              <div className="tweet-icons">
+                <span className="coment">
+                  <Coment />0
+                </span>{" "}
+                <span className="retweet">
+                  <Retweet />0
+                </span >{" "}
+                <span className="like">
+                  <Like />0
+                </span>{" "}
+                <Upload />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="user-tweet">
+          <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" className="tweet-author" />
+          <div className="tweet-body">
+            <div className="tweet-header">
+              <span className="text-slate-200 font-bold">Username</span>
+              <span>@usertag</span>
+              <span>1 h</span>
+            </div>
+            <div className="tweet-content">
+              <p className="tweet">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum numquam voluptatem nostrum atque vel!
+                Quasi ad optio velit obcaecati veniam odio aliquid sunt blanditiis, minus facilis dignissimos
+                repudiandae saepe sed!
+              </p>
+              <div className="tweet-icons">
+                <span className="coment">
+                  <Coment />0
+                </span>{" "}
+                <span className="retweet">
+                  <Retweet />0
+                </span >{" "}
+                <span className="like">
+                  <Like />0
+                </span>{" "}
+                <Upload />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <Aside />
